759b69a45e16deb1fc78de33403bcb7b57daee6a
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-02-17 12:42:41 +0800

    Added a frame which says scanning complete

--- QR_Code_Scanner.py
+++ QR_Code_Scanner.py
@@ -14,8 +14,12 @@ cap.set(4, 480)
 while True:
     success, img = cap.read()
     for barcode in decode(img):
-
         my_data = barcode.data.decode("utf-8")
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,(0,255,0),5)
+        pts2 = barcode.rect
+        cv2.putText(img,"SCANNING COMPLETE YEEHAW",(pts2[0],pts2[1]),cv2.FONT_HERSHEY_COMPLEX,0.9,(0,255,0),2)
         fh = open("Personal_Data.txt", "w")
         fh.write(f"{my_data}\n\n\n\nDate and Time read: {now}")
         print("Scanning Done")
